191296637a403224b2146fb3d4a18a4427cc10b5
James Grande <james.grande6@gmail.com>
2021-07-06 07:44:28 -1000

    Add playRound function

--- script.js
+++ script.js
@@ -14,3 +14,40 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+        return "tie";
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            console.log("You lose! Paper beats Rock");
+            return "lose";
+        }
+        if (computerSelection === "scissors") {
+            console.log("You win! Rock beats Scissors");
+            return "win";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            console.log("You lose! Scissors beats paper");
+            return "lose";
+        }
+        if (computerSelection === "rock") {
+            console.log("You win! Paper beats Rock");
+            return "win";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            console.log("You lose! Rock beats scissors");
+            return "lose";
+        }
+        if (computerSelection === "paper") {
+            console.log("You win! Scissors beats paper");
+            return "win";
+        }
+    }
+}
+
